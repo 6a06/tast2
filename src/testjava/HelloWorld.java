f3e5d265426c1342460104a547c4dcae0224c40e
6A06 <6A06@DESKTOP-QRBQSNF>
2022-03-07 12:37:03 +0900

    ab

--- src/testjava/HelloWorld.java
+++ src/testjava/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("World");
+		int a=10;
+		int b=20;
+		System.out.println(a*b);
 	}
 
 }
